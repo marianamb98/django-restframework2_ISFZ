63ed0945e6476fa6aca415cdbec20b6d1531aa52
marianamb98 <marianabmamani98@gmail.com>
2025-11-18 18:33:34 -0300

    Enhance URL configuration with API documentation
    Added Swagger and ReDoc UI paths for API documentation.

--- ejercicios_practica/marvel/marvel/urls.py
+++ ejercicios_practica/marvel/marvel/urls.py
@@ -33,5 +33,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('e-commerce/',include('e_commerce.api.urls')),
+    # Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    # ReDoc UI
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
 
